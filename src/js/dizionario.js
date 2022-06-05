478c243efdb3b4766351381b47f7a6aef01d99fe
alem <al.ma0305@protonmail.com>
2022-06-05 22:40:34 +0200

    completa

--- src/js/dizionario.js
+++ src/js/dizionario.js
@@ -1,10 +1,10 @@
 export default [
     'mare','sole','maestro','martello','pirata','ginocchio','mortaio','gesto','mulino','puntura',
-    'vaccino','ingoio','poppa','il','prua','quaquaraqua','maresciallo','miracolo','mulino','da','il',
+    'vaccino','iginio','poppa','il','prua','quaquaraqua','maresciallo','miracolo','mulino','da','il',
     'xenofobo','webpack','topa','tostapane','gatto','cavallo','vichingo','bello','bicchiere','vigoroso',
     'ferita','zattera','zio','dio','gustoso','orecchini','testardo','albergo','bicchiere','abaco','degli',
-    'deodorante', 'zingaro','fifone','fegato','pippa','pescivendolo','karate','judoka','hotel','figa','gin',
-    'kekka','gino','leccalecca','frocione','pupazzo','gallina','zotico','zecca','gioacchino','gara','maratona',
+    'deodorante', 'zingaro','fifone','fegato','pippo','pescivendolo','karate','judoka','hotel','fifa','gin',
+    'kino','gino','leccalecca','fagiano','pupazzo','gallina','zotico','zecca','gioacchino','gara','maratona',
     'roma','milano','napoli','banzai','sedia','clown','we','xena','borotalco','fuoco','pizza','pezzo','panettone',
     'diocleziano'
 ]
